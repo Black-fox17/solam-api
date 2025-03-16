6d8d904699de1ea3a0cde801d009c8765c7578ec
AbdulSalam Ayeleru <93570850+Black-fox17@users.noreply.github.com>
2025-03-16 12:39:32 +0100

    Remove Docker configuration files and unused scripts

--- main.py
+++ main.py
@@ -25,42 +25,6 @@ async def probe():
     return {"message": "I am the Python FastAPI API responding"}
 
 
-# # REGISTER EXCEPTION HANDLERS
-# @app.exception_handler(HTTPException)
-# async def http_exception(request: Request, exc: HTTPException):
-#     """HTTP exception handler"""
-
-#     return JSONResponse(
-#         status_code=exc.status_code,
-#         content={
-#             "status": False,
-#             "status_code": exc.status_code,
-#             "message": exc.detail,
-#         },
-#     )
-
-
-
-
-# @app.exception_handler(RequestValidationError)
-# async def validation_exception(request: Request, exc: RequestValidationError):
-#     """Validation exception handler"""
-
-#     errors = [
-#         {"loc": error["loc"], "msg": error["msg"], "type": error["type"]}
-#         for error in exc.errors()
-#     ]
-
-#     return JSONResponse(
-#         status_code=422,
-#         content={
-#             "status": False,
-#             "status_code": 422,
-#             "message": "Invalid input",
-#             "errors": errors,
-#         },
-#     )
-
 
 if __name__ == "__main__":
     uvicorn.run("main:app",port = 8000, reload=True)
